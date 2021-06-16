6880331e68f7085d4c63d011ff3de66e9b70d4a3
Jsilwembe <jsilwembe@gmail.com>
2021-06-16 04:52:24 +0200

    new changes 2

--- police-bulk-sms/src/environments/environment.prod.ts
+++ police-bulk-sms/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAgSyHDQThuW2PS5fdRptt8gOGO9hk4uwM',
+    authDomain: 'policebulksms.firebaseapp.com',
+    projectId: 'policebulksms',
+    storageBucket: 'policebulksms.appspot.com',
+    messagingSenderId: '832093369794',
+    appId: '1:832093369794:web:591cc9243f3a2cc8ed925b',
+    measurementId: 'G-H1GN3J358T'
+  }
 };
